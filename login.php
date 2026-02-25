c911a2ca6757a85be25a7822f71d22dd04fac89d
Маханов Матвей Васильевич <mmv@ktt.ru>
2026-02-25 15:27:37 +0700

    [project] login page logic

--- login.php
+++ login.php
@@ -1,3 +1,4 @@
+<?php session_start(); ?>
 <!DOCTYPE html>
 <html lang="ru">
 <head>
@@ -7,5 +8,23 @@
 </head>
 <body>
   <?php require_once "components/header.php" ?>
+  <main class="main">
+    <div class="container">
+      <form action="/auth/login.php" method="post">
+        <div class="form-item">
+          <label for="login" class="form__label">Логин</label>
+          <input type="text" class="form__input" id="login" name="login" required value="<?= isset($_SESSION['input']['login']) ? $_SESSION['input']['login'] : '' ?>">
+          <small><?= isset($_SESSION['errors']['login']) ? $_SESSION['errors']['login'] : '' ?></small>
+        </div>
+        <div class="form-item">
+          <label for="password" class="form__label">Пароль</label>
+          <input type="password" class="form__input" id="password" name="password" required minlength="8">
+          <small><?= isset($_SESSION['errors']['password']) ? $_SESSION['errors']['password'] : '' ?></small>
+        </div>
+        <button type="submit">Войти</button>
+      </form>
+      <?php unset($_SESSION['errors'], $_SESSION['input']); ?>
+    </div>
+  </main>
 </body>
 </html>
