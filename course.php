fa87530a2885f83a5554facd108c5f1d5788db5b
Маханов Матвей Васильевич <mmv@ktt.ru>
2026-03-17 19:07:39 +0700

    courses

--- course.php
+++ course.php
@@ -10,22 +10,23 @@ require_once "db/courses/show.php";
     <meta name="viewport"
         content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title><?= $course['name'] ?></title>
+    <title><?= $course_data['name'] ?></title>
 </head>
 
 <body>
     <?php require_once "components/header.php"; ?>
-    <?php print_r($course); ?>
+    <?php echo "<pre>";print_r($course_data);echo "</pre>"; ?>
+    <?php echo "<pre>"; ($reviews) ? print_r($reviews) : print_r('Нет отзывов'); echo "</pre>"; ?>
     <?php require_once "db/courses/check-bid.php"; ?>
     <?php if($isAvailable): ?>
     <form action="db/bids/create.php" method="post">
         <?php if (isset($_SESSION['user_login'])): ?>
             <input type="hidden" name="user" value="<?= htmlspecialchars($_SESSION['user_id']) ?>">
         <?php endif; ?>
-        <input type="hidden" name="course" value="<?= htmlspecialchars($course['course_id']) ?>">
+        <input type="hidden" name="course" value="<?= htmlspecialchars($course_data['course_id']) ?>">
         <button type="submit">Записаться на курс</button>
     </form>
-    <?php else: echo ""; endif; ?>
+    <?php else: echo "<p>Вы записаны на курс</p>"; endif; ?>
 </body>
 
 </html>
--- db/courses/show.php
+++ db/courses/show.php
@@ -6,8 +6,19 @@ require_once __DIR__ . "/../config.php";
 $stmt = $pdo->prepare("SELECT * FROM courses WHERE course_id = ? LIMIT 1");
 $stmt->execute([$course]);
 
-if ($course = $stmt->fetch()) {
-
+if ($course_data = $stmt->fetch()) {
+    $stmt = $pdo->prepare("
+SELECT r.review_id, r.course_id, r.user_id, r.content, c.name, c.description, u.login, r.created_at AS date 
+FROM reviews AS r
+JOIN courses AS c ON r.course_id = c.course_id
+JOIN users AS u ON r.user_id = u.user_id
+WHERE r.course_id = ?");
+    $stmt->execute([$course]);
+    if ($reviews_data = $stmt->fetchAll()) {
+        $reviews = $reviews_data;
+    } else {
+        $reviews = null;
+    }
 } else {
     http_response_code(404);
     include "404.php";
